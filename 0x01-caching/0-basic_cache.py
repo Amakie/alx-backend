002bb4623add9516ccba6fa6d41bcba297574a26
Constance Amatey <consamate57@gmail.com>
2024-04-03 23:21:30 +0000

    indexing

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,15 +7,13 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """BasicCache class inheriting from parent class BaseCaching"""
 
+    def put(self, key, item):
+        """function that adds item in cache"""
+        if key or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
 
-def put(self, key, item):
-    """function that adds item in cache"""
-    if key or item is None:
-        pass
-    else:
-        self.cache_data[key] = item
-
-
-def get(self, key):
-    """Retrieves an item by key"""
-    return self.cache_data.get(key, None)
+    def get(self, key):
+        """Retrieves an item by key"""
+        return self.cache_data.get(key, None)
