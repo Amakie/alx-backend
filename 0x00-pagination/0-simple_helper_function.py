3514114abcda7921a5372f395a816d986c78ece8
Constance Amatey <consamate57@gmail.com>
2024-04-03 16:17:48 +0000

    capitalized function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ function that takes two integer arguments page and page_size"""
 
-from typing import tuple
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of the start and end pages"""
     return (((page-1) * page_size), (page * page_size))
