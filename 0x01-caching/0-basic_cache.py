59da7929aa95810675ec7c22bcf13d6bf4000219
Constance Amatey <consamate57@gmail.com>
2024-04-03 23:00:49 +0000

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Basic caching system"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class inheriting from parent class BaseCaching"""
+
+
+def put(self, key, item):
+    """function that adds item in cache"""
+    if key or item is None:
+        return ()
+    self.cache_data[key] = item
+
+
+def get(self, key):
+    """Retrieves an item by key"""
+    return self.cache_data.get(key, None)
