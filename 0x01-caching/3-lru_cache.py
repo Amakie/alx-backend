64547d98c74192aeb3533f1f9ea69d2171a0eed1
Constance Amatey <consamate57@gmail.com>
2024-04-05 17:53:36 +0000

    task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,22 +8,15 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """_summary_
-    """
+    """LRUCache class"""
 
     def __init__(self):
-        """_summary_
-        """
+        """initializing self"""
         super().__init__()
         self.usedKeys = []
 
     def put(self, key, item):
-        """_summary_
-
-        Args:
-                        key (_type_): _description_
-                        item (_type_): _description_
-        """
+        """put function"""
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.usedKeys:
@@ -37,11 +30,7 @@ class LRUCache(BaseCaching):
                 print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
-
-        Args:
-                        key (_type_): _description_
-        """
+        """return the value in self.cache_data linked to key"""
         if key is not None and key in self.cache_data.keys():
             self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
             return self.cache_data.get(key)
